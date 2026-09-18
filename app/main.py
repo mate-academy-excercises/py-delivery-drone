410a41a0255bc46a437d63ed9d56cee781d33d22
Ryszard Kluz <Ryszard.kluz.fm@gmail.com>
2026-09-18 11:11:15 +0200

    feat: add drones classes and logic accordingly

--- app/main.py
+++ app/main.py
@@ -2,4 +2,57 @@ class Cargo:
     def __init__(self, weight: int) -> None:
         self.weight = weight
 
-# write your code here
+
+class BaseRobot:
+    def __init__(self, name: str, weight: int,
+                 coords: tuple = (0, 0)) -> None:
+
+        self.name = name
+        self.weight = weight
+        self.coords = list(coords)
+
+    def go_forward(self, steps: int = 1) -> None:
+        self.coords[1] += steps
+
+    def go_back(self, steps: int = 1) -> None:
+        self.coords[1] -= steps
+
+    def go_right(self, steps: int = 1) -> None:
+        self.coords[0] += steps
+
+    def go_left(self, steps: int = 1) -> None:
+        self.coords[0] -= steps
+
+    def get_info(self) -> str:
+        return f"Robot: {self.name}, Weight: {self.weight}"
+
+
+class FlyingRobot(BaseRobot):
+    def __init__(self, name: str, weight: int,
+                 coords: tuple = (0, 0, 0)) -> None:
+
+        super().__init__(name, weight, coords)
+
+    def go_up(self, steps: int = 1) -> None:
+        self.coords[2] += steps
+
+    def go_down(self, steps: int = 1) -> None:
+        self.coords[2] -= steps
+
+
+class DeliveryDrone(FlyingRobot):
+
+    def __init__(self, name: str, weight: int, max_load_weight: int,
+                 current_load: Cargo | None = None,
+                 coords: tuple = (0, 0, 0)) -> None:
+
+        super().__init__(name, weight, coords)
+        self.max_load_weight = max_load_weight
+        self.current_load = current_load
+
+    def hook_load(self, cargo: Cargo) -> None:
+        if not self.current_load and not cargo.weight > self.max_load_weight:
+            self.current_load = cargo
+
+    def unhook_load(self) -> None:
+        self.current_load = None
